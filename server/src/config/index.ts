ca4d29ee2d55fea1370957eceeeb19d914296df0
Goran Tenic <tenic.g@gmail.com>
2021-10-02 21:33:52 +0200

    loading env variables for pg library

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,15 +1,33 @@
 import "dotenv";
+import assert from "assert";
 
 const ENV = process.env.NODE_ENV || "development";
+
+const { PGDATABASE, PGUSER, PGHOST, PGPASSWORD, PGPORT, SESSION_SECRET } =
+  process.env;
+
+assert(PGDATABASE, "env variable PGDATABASE must be defined");
+assert(PGUSER, "env variable PGUSER must be defined");
+assert(PGHOST, "env variable PGHOST must be defined");
+assert(PGPASSWORD, "env variable PGPASSWORD must be defined");
+assert(PGPORT, "env variable PGPORT must be defined");
+assert(SESSION_SECRET, "env variable SESSION_SECRET must be defined");
 
 const appConfig = {
   ENV,
   isDev: ENV === "development",
   port: 3000,
-  sessionSecret: process.env.SESSION_SECRET,
+  sessionSecret: SESSION_SECRET,
   cookieName: "connect.sid",
   frontend: "http://localhost:8080",
   //   redisPort: ENV === "test" ? 6380 : 6379,
+  sql: {
+    PGDATABASE,
+    PGUSER,
+    PGHOST,
+    PGPASSWORD,
+    PGPORT,
+  },
 } as const;
 
 export default appConfig;
