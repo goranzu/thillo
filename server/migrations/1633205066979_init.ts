8a7fa418558987981200a07becf13a027f7c2b79
Goran Tenic <tenic.g@gmail.com>
2021-10-03 22:07:07 +0200

    migrations
    added the following:
    - boards column
    - lists column
    - unique index on boards.name and boards.creatorId

--- server/migrations/1633205066979_init.ts
+++ server/migrations/1633205066979_init.ts
@@ -1,32 +1,93 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { MigrationBuilder, ColumnDefinitions } from "node-pg-migrate";
+import {
+  MigrationBuilder,
+  ColumnDefinitions,
+  PgLiteral,
+} from "node-pg-migrate";
 
-export const shorthands: ColumnDefinitions | undefined = undefined;
+export const shorthands: ColumnDefinitions | undefined = {
+  createdAt: {
+    type: "TIMESTAMP",
+    notNull: true,
+    default: new PgLiteral("current_timestamp"),
+  },
+};
+
+function textColumn(notNull: boolean) {
+  return {
+    type: "TEXT",
+    notNull,
+  };
+}
+
+const tableNames = {
+  users: "users",
+  boards: "boards",
+  lists: "lists",
+};
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
-  pgm.createTable("users", {
+  pgm.createTable(tableNames.users, {
     id: "id",
     email: {
-      type: "TEXT",
-      notNull: true,
+      ...textColumn(true),
       unique: true,
     },
-    password: {
-      type: "TEXT",
-      notNull: true,
-    },
+    firstName: textColumn(true),
+    lastName: textColumn(true),
+    password: textColumn(true),
     passwordSalt: {
       type: "BYTEA",
       notNull: true,
     },
-    createdAt: {
-      type: "timestamp",
-      notNull: true,
-      default: pgm.func("current_timestamp"),
-    },
+    createdAt: "createdAt",
   });
+
+  pgm.createTable(tableNames.boards, {
+    id: "id",
+    name: textColumn(true),
+    isPrivate: {
+      type: "BOOLEAN",
+      notNull: true,
+      default: false,
+    },
+    description: textColumn(false),
+    creatorId: {
+      type: "INTEGER",
+      notNull: true,
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+      references: '"users"',
+    },
+    createdAt: "createdAt",
+  });
+
+  pgm.createTable(tableNames.lists, {
+    id: "id",
+    name: textColumn(true),
+    description: textColumn(false),
+    boardId: {
+      type: "INTEGER",
+      notNull: true,
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+      references: '"boards"',
+    },
+    creatorId: {
+      type: "INTEGER",
+      notNull: true,
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+      references: '"users"',
+    },
+    createdAt: "createdAt",
+  });
+
+  pgm.createIndex(tableNames.boards, ["name", "creatorId"], { unique: true });
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-  pgm.dropTable("users");
+  pgm.dropTable(tableNames.lists);
+  pgm.dropTable(tableNames.boards);
+  pgm.dropTable(tableNames.users);
 }
