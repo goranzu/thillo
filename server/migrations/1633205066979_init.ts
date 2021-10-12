f4e136b3bbfd96ab8bb8991944801d19f8eaf737
Goran Tenic <tenic.g@gmail.com>
2021-10-12 21:02:24 +0200

    more columns

--- server/migrations/1633205066979_init.ts
+++ server/migrations/1633205066979_init.ts
@@ -24,6 +24,10 @@ const tableNames = {
   users: "users",
   boards: "boards",
   lists: "lists",
+  boardMembers: "boardMembers",
+  cards: "cards",
+  cardComments: "cardComments",
+  cardAttachments: "cardAttachments",
 };
 
 export async function up(pgm: MigrationBuilder): Promise<void> {
@@ -43,15 +47,100 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     createdAt: "createdAt",
   });
 
-  pgm.createTable(tableNames.boards, {
-    id: "id",
-    name: textColumn(true),
-    isPrivate: {
-      type: "BOOLEAN",
-      notNull: true,
-      default: false,
+  pgm.createTable(
+    tableNames.boards,
+    {
+      id: "id",
+      name: textColumn(true),
+      isPrivate: {
+        type: "BOOLEAN",
+        notNull: true,
+        default: false,
+      },
+      description: textColumn(false),
+      creatorId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"users"',
+      },
+      createdAt: "createdAt",
     },
+    {
+      constraints: {
+        unique: ["name", "creatorId"],
+      },
+    },
+  );
+
+  pgm.createTable(
+    tableNames.lists,
+    {
+      id: "id",
+      name: textColumn(true),
+      description: textColumn(false),
+      boardId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"boards"',
+      },
+      creatorId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"users"',
+      },
+      createdAt: "createdAt",
+    },
+    {
+      constraints: {
+        unique: ["name", "boardId"],
+      },
+    },
+  );
+
+  pgm.createTable(
+    tableNames.boardMembers,
+    {
+      memberId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"users"',
+      },
+      boardId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"boards"',
+      },
+      createdAt: "createdAt",
+    },
+    {
+      constraints: {
+        primaryKey: ["memberId", "boardId"],
+      },
+    },
+  );
+
+  pgm.createTable(tableNames.cards, {
+    id: "id",
+    title: textColumn(true),
     description: textColumn(false),
+    coverUrl: textColumn(false),
+    listId: {
+      type: "INTEGER",
+      notNull: true,
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
+      references: '"lists"',
+    },
     creatorId: {
       type: "INTEGER",
       notNull: true,
@@ -62,31 +151,56 @@ export async function up(pgm: MigrationBuilder): Promise<void> {
     createdAt: "createdAt",
   });
 
-  pgm.createTable(tableNames.lists, {
-    id: "id",
-    name: textColumn(true),
-    description: textColumn(false),
-    boardId: {
-      type: "INTEGER",
-      notNull: true,
-      onDelete: "CASCADE",
-      onUpdate: "CASCADE",
-      references: '"boards"',
+  pgm.createTable(
+    tableNames.cardComments,
+    {
+      authorId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"users"',
+      },
+      cardId: {
+        type: "INTEGER",
+        notNull: true,
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+        references: '"cards"',
+      },
+      text: textColumn(true),
+      createdAt: "createdAt",
     },
-    creatorId: {
+    {
+      constraints: {
+        primaryKey: ["authorId", "cardId"],
+      },
+    },
+  );
+
+  pgm.createTable(tableNames.cardAttachments, {
+    id: "id",
+    url: textColumn(true),
+    cardId: {
       type: "INTEGER",
       notNull: true,
       onDelete: "CASCADE",
       onUpdate: "CASCADE",
-      references: '"users"',
+      references: '"cards"',
     },
     createdAt: "createdAt",
   });
 
-  pgm.createIndex(tableNames.boards, ["name", "creatorId"], { unique: true });
+  //   pgm.createIndex(tableNames.boards, ["name", "creatorId"], { unique: true });
+  //   pgm.createIndex(tableNames.lists, ["name", "boardId"], { unique: true });
+  //   pgm.addConstraint(tableNames.boardMembers, )
 }
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable(tableNames.cardAttachments);
+  pgm.dropTable(tableNames.cardComments);
+  pgm.dropTable(tableNames.boardMembers);
+  pgm.dropTable(tableNames.cards);
   pgm.dropTable(tableNames.lists);
   pgm.dropTable(tableNames.boards);
   pgm.dropTable(tableNames.users);
